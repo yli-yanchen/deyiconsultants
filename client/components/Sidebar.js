bad11fba0849bed253d1d84d8fbc594509f69a28
Cheryl Lee <cheryllee@cheryls-mbp.lan>
2024-03-17 14:02:00 -0700

    create sidebar components

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, link, navLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { SiShopware } from "react-icons/si";
 
+const Sidebar = (props) => {
+  const [menu, setMenu] = useState("");
 
-const Sidebar = () => {
-  const activeMenu = true;
+  const activeLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
+  const normalLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200";
 
-  
+  const navigate = useNavigate();
 
   return (
-    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
-      {" "}
-      {activeMenu && (
-        <div className="flex justify-between items-center">
-          {" "}
-          <Link to="/" onClick={() => {}} className="items-center">
-            <SiShopware /> <span>Profile</span>
-          </Link>
-        </div>
-      )}{" "}
+    <div className="h-screen w-72 bg-priwhite text-priblue fixed flex-col top-0 left-0 m-0 flex shadow-lg">
+      <div className="flex flex-col justify-between items-center mt-40">
+        <Link to="/profile" onClick={() => {}} className="items-center">
+          <span className="flex top-4 left-2">{props.user.firstName}</span>
+        </Link>
+      </div>
+      <div>
+        <ul>
+          <li className="flex flex-col mt-20 ml-20" onClick={() => setMenu("Profile")}>
+            <Link style={{ textDecoration: "none" }} to={"/profile"}>
+              Profile{menu === "Profile" ? <hr /> : <></>}
+            </Link>
+          </li>
+          <li className="flex flex-col mt-10 ml-20" onClick={() => setMenu("Project")}>
+            <Link style={{ textDecoration: "none" }} to={"/profile"}>
+              Project{menu === "Project" ? <hr /> : <></>}
+            </Link>
+          </li>
+          <li className="flex flex-col mt-10 ml-20" onClick={() => setMenu("Payment")}> 
+            <Link style={{ textDecoration: "none" }} to={"/profile"}>
+              Payment{menu === "Payment" ? <hr className="text-bold" /> : <></>}
+            </Link>
+          </li>
+          <li className="flex flex-col mt-10 ml-20" onClick={() => setMenu("Message")}> 
+            <Link style={{ textDecoration: "none" }} to={"/profile"}>
+              Message{menu === "Message" ? <hr/> : <></>}
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <Link to="/profile" onClick={() => {}} className="items-center">
+          <span className="flex flex-col mt-10 ml-20">
+            Logout{menu === "Logout" ? <hr /> : <></>}
+          </span>
+        </Link>
+      </div>
     </div>
   );
 };
--- client/subpage/Profile.js
+++ client/subpage/Profile.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import homeImage from "../../docs/assets/images/homepagePicNoText.png";
+import { HiOutlineMenu } from "react-icons/hi";
 import axios from "../hook/axios";
 
 const Profile = () => {
   const navigate = useNavigate();
-  const [currUser, setCurrUser] = useState(null);
+  const [currUser, setCurrUser] = useState("");
+  const [activeSideBar, setActiveSideBar] = useState(false);
 
   useEffect(() => {
     const userid = localStorage.getItem("userid");
@@ -19,7 +21,7 @@ const Profile = () => {
             Authorization: `Bearer ${userid}`,
           },
         });
-        console.log(">>> getuserdata: ", getuserdata.data);
+        setCurrUser(getuserdata.data);
       } catch (error) {
         console.error("Error fetching user data:", error);
       }
@@ -29,8 +31,8 @@ const Profile = () => {
   }, [navigate]);
 
   return (
-    <div
-      className="h-screen flex flex-row justify-center items-center"
+    <main
+      className="h-screen flex flex-row justify-center items-center relative"
       style={{
         backgroundImage: `url(${homeImage})`,
         backgroundSize: "cover",
@@ -38,9 +40,26 @@ const Profile = () => {
         backgroundColor: "rgba(255, 255, 255, 0.05)",
       }}
     >
-      <Sidebar />
-      <h1 className="flex items-center"> {currUser} </h1>
-    </div>
+      <div className="fixed top-4 left-4 h-1.5 w-2 items-center">
+        <HiOutlineMenu
+          className="font-bold size-6"
+          onClick={() =>
+            activeSideBar ? setActiveSideBar(false) : setActiveSideBar(true)
+          }
+        />
+        <span className="text-priblue"> Hi, {currUser.firstName}</span>
+      </div>
+      <div>
+        {activeSideBar ? (
+          <div className="2-72 fixed bg-priwhite">
+            <Sidebar user={currUser} />
+          </div>
+        ) : (
+          <div className="w-0"></div>
+        )}
+      </div>
+      {/* <Sidebar user={currUser}/> */}
+    </main>
   );
 };
 
