c9ea111eccd732d1034df4fd45dba4516524a2f8
Cheryl Lee <yli.yanchen@gmail.com>
2024-05-25 12:08:33 -0700

    adjust the routes in the front-end. have dashboard routes working

--- server/models/projectModel.js
+++ server/models/projectModel.js
@@ -1,27 +1,39 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const dotenv = require("dotenv");
-const User = require("./userModel");
+const dotenv = require('dotenv');
+const User = require('./userModel');
+const { date } = require('joi');
 dotenv.config();
 
-const projectSchema = new Schema({
-  name: { type: String, required: true },
-  address: { type: String, required: true },
-  status: { type: String, required: true },
-  clientid: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
-  description: { type: String },
-  amount: {type: Number},
-  paid: {type: Number},
-  reimbersed: {type: Number},
-  balance: {type: Number},
-  document: [{
-    name: {type: String, required: true},
-    uploadedAt: {type: Date, default: Date.now},
-    uploadedBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-  }],
-}, {
-  timestamps: true
-});
+const projectSchema = new Schema(
+  {
+    ID: { type: String, required: true },
+    Name: { type: String, required: true },
+    Address: { type: String, required: true },
+    City: { type: String, required: true },
+    Client: { type: String, required: true },
+    ProjectType: { type: String, required: true },
+    StartDate: { type: Date },
+    EndDate: { type: Date },
+    Status: { type: String, required: true },
 
-const Project = mongoose.model("Project", projectSchema);
+    ContractAmount: { type: Number },
+    Reimbersement: { type: Number },
+    PaidAmount: { type: Number },
+    BalanceAmount: { type: Number },
+
+    Document: [
+      {
+        name: { type: String, required: true },
+        uploadedAt: { type: Date, default: Date.now },
+        uploadedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Project = mongoose.model('Project', projectSchema);
 module.exports = { Project };
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require("bcryptjs");
-const dotenv = require("dotenv");
-const Project = require("./projectModel");
+const bcrypt = require('bcryptjs');
+const dotenv = require('dotenv');
+const Project = require('./projectModel');
 dotenv.config();
 
 const SALT_FACTOR = Number(process.env.SALT_FACTOR);
@@ -11,36 +11,36 @@ console.log(
 );
 
 const ROLE = {
-  ADMIN: "admin",
-  BASIC: "basic",
-}
+  ADMIN: 'admin',
+  BASIC: 'basic',
+};
 
 const userSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  address: { type: String },
   role: { type: String, enum: [ROLE.ADMIN, ROLE.BASIC], default: ROLE.BASIC },
   refreshToken: { type: [String] },
-  projectid: { type: mongoose.Schema.Types.ObjectId, ref: "Project" },
+  projectid: { type: mongoose.Schema.Types.ObjectId, ref: 'Project' },
 });
 
-userSchema.pre("save", function (next) {
+userSchema.pre('save', function (next) {
   const user = this;
-  console.log(">>> Non-hashed User password: ", user.password);
+  console.log('>>> Non-hashed User password: ', user.password);
 
   // Check if the password field is modified or the document is new
   // The isModified() method is not a user-defined function; it's a built-in method provided by Mongoose to check if a specific field in a document has been modified.
-  if (!user.isModified("password")) return next();
+  if (!user.isModified('password')) return next();
 
   bcrypt.hash(user.password, SALT_FACTOR, function (err, hash) {
     if (err) return next(err);
     user.password = hash;
-    console.log(">>> Hashed user password: ", user.password);
+    console.log('>>> Hashed user password: ', user.password);
     return next();
   });
 });
 
 const User = mongoose.model('User', userSchema);
 module.exports = { User };
-
