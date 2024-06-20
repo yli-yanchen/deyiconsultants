e6aa7f6db77842b3619335723bf0b774b3452a86
Cheryl Lee <yli.yanchen@gmail.com>
2024-06-19 21:33:30 -0700

    update dependencies, get recent data model

--- server/models/projectModel.js
+++ server/models/projectModel.js
@@ -1,27 +1,58 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const dotenv = require("dotenv");
-const User = require("./userModel");
+const dotenv = require('dotenv');
+const User = require('./userModel');
+const { date, types } = require('joi');
 dotenv.config();
 
-const projectSchema = new Schema({
-  name: { type: String, required: true },
-  address: { type: String, required: true },
-  status: { type: String, required: true },
-  clientid: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
-  description: { type: String },
-  amount: {type: Number},
-  paid: {type: Number},
-  reimbersed: {type: Number},
-  balance: {type: Number},
-  document: [{
-    name: {type: String, required: true},
-    uploadedAt: {type: Date, default: Date.now},
-    uploadedBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-  }],
-}, {
-  timestamps: true
-});
+const projectSchema = new Schema(
+  {
+    ID: { type: String, required: true },
+    Name: { type: String, required: true },
+    Address: { type: String, required: true },
+    City: { type: String, required: true },
+    ClientFirstName: { type: String, required: true },
+    ClientLastName: { type: String, required: true },
+    ProjectType: { type: String, required: true },
+    StartDate: { type: Date },
+    EndDate: { type: Date },
+    Status: { type: String, required: true },
 
-const Project = mongoose.model("Project", projectSchema);
+    ContractAmount: { type: Number, required: true },
+    Reimbersement: { type: Number, required: true },
+    IncomingAmount: { type: Number, required: true },
+    BalanceAmount: { type: Number, required: true },
+
+    Engineer: [
+      {
+        Name: { type: String, required: true, default: 'Jeff' },
+        OutcomingAmount: { type: Number, required: true, default: 0 },
+        Deadline: {
+          type: Date,
+          required: true,
+          default: function () {
+            return this.StartDate;
+          },
+        },
+      },
+    ],
+
+    CreatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    ClientID: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    EngineerID: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+
+    Document: [
+      {
+        name: { type: String, required: true },
+        uploadedAt: { type: Date, default: Date.now },
+        uploadedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Project = mongoose.model('Project', projectSchema);
 module.exports = { Project };
